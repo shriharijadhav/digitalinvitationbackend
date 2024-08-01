d406cbaba7a805a739af56f188ddd6800147471b
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-01 08:30:54 +0530

    code changes

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.FRONTEND_BASE_URL,
-    credentials: true,
-  }));
+app.use(cors());
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
